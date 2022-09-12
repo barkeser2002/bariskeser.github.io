8906403f492884e385516ba76c290744b265ee49
barkeser2002 <bkeser10@gmail.com>
2022-09-12 04:55:45 +0300

    unused vars delete

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Greeter from '../views/Greeter';
 import Diploma from '../views/Diploma';
 import { PageLink, TabLink } from '../models';
-import { jsOrange, midBlue, green, pink } from '../theme/colors';
+import { jsOrange, green } from '../theme/colors';
 
 import staticData from './json/static.json';
 import contactData from './json/contact.json';
 import planlarData from './json/planlar.json';
-import baData from './json/ba.json';
-import pkg from '../../package.json';
 
 import { calculateDiplomaProgress } from '../utils';
 
